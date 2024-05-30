3f468bb1d4b86f020774353c05166ef0d43de25e
unknown <gracek_608@wp.pl>
2024-05-30 16:29:19 +0200

    working auth, simple models, docs

--- hydroapi/models.py
+++ hydroapi/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class HydroponicSystem(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hydroponic_systems')
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class Measurement(models.Model):
+    hydroponic_system = models.ForeignKey(HydroponicSystem, on_delete=models.CASCADE, related_name='measurements')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    ph = models.DecimalField(max_digits=6, decimal_places=2)
+    temperature = models.DecimalField(max_digits=6, decimal_places=2)
+    tds = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"placeholder"
--- hydroponics/settings.py
+++ hydroponics/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
     "hydroapi"
 ]
 
@@ -54,6 +56,16 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 
 ROOT_URLCONF = "hydroponics.urls"
 
